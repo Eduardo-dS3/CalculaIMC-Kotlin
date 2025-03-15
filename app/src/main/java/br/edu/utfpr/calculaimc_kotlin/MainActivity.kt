26c8affc9b5c7c8a7f35dabdbeb4aeb736591fe9
Eduardo dos Santos <eduardo.santos.3538@gmail.com>
2025-03-15 10:26:36 -0300

    Inicio

--- app/src/main/java/br/edu/utfpr/calculaimc_kotlin/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/calculaimc_kotlin/MainActivity.kt
@@ -1,20 +1,61 @@
 package br.edu.utfpr.calculaimc_kotlin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etPeso: EditText
+    private lateinit var etAltura: EditText
+    private lateinit var tvResultado: TextView
+    private lateinit var btCalcular: Button
+    private lateinit var btLimpar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        etPeso = findViewById(R.id.etPeso)
+        etAltura = findViewById(R.id.etAltura)
+        tvResultado = findViewById(R.id.tvResultado)
+        btCalcular = findViewById(R.id.btCalcular)
+        btLimpar = findViewById(R.id.btLimpar)
+
+        btCalcular.setOnClickListener {
+            btCalcularOnCLick()
         }
+
+        btLimpar.setOnClickListener {
+            btLimparOnClick()
+        }
+    }
+
+    private fun btLimparOnClick() {
+        etPeso.setText("")
+        etAltura.setText("")
+        tvResultado.setText("0,0")
+        etPeso.requestFocus()
+    }
+
+    private fun btCalcularOnCLick() {
+
+        if (etPeso.text.toString().isEmpty()) {
+            etPeso.error = "Campo peso deve ser informado."
+            return
+        }
+
+        if (etAltura.text.toString().isEmpty()) {
+            etAltura.error = "Campo altura deve ser informado."
+            return
+        }
+
+        val peso = etPeso.text.toString().toDouble()
+        val altura = etAltura.text.toString().toDouble()
+        val imc = peso / altura.pow(2)
+        tvResultado.text = "%.2f".format(imc)
     }
 }
